215eda2e34de04d9782836db47d9d847b3aec992
YoussefSamara <99098452+YoussefSamara@users.noreply.github.com>
2024-10-12 03:32:11 +0300

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
+const correctAnswer = "4";
+const userAnswer = document.querySelector('input[name="quiz"]:checked');
 function checkAnswer() {
-    const correctAnswer = "4";
-    const userAnswer = document.querySelector('input[name="quiz"]:checked');
     if (!userAnswer) {
         document.getElementById('feedback').textContent = "Please select an answer.";
         return;
